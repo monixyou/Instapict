dac04a99f52fa945e4e16b455d37e5657fa2369c
Monica Bui <monicahb@fb.com>
2020-07-07 15:42:34 -0700

    Segue to timeline controller

---
+++ Instapict/View Controllers/TimelineViewController.h
@@ -0,0 +1,17 @@
+//
+//  TimelineViewController.h
+//  Instapict
+//
+//  Created by Monica Bui on 7/7/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TimelineViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instapict/View Controllers/TimelineViewController.m
@@ -0,0 +1,32 @@
+//
+//  TimelineViewController.m
+//  Instapict
+//
+//  Created by Monica Bui on 7/7/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import "TimelineViewController.h"
+
+@interface TimelineViewController ()
+
+@end
+
+@implementation TimelineViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
