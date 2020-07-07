eb9fe9199182c458d7f487ac8bd730e116322c76
Monica Bui <monicahb@fb.com>
2020-07-06 22:06:11 -0700

    Parse pod files

---
+++ Pods/Target Support Files/Pods-Instapict-InstapictUITests/Pods-Instapict-InstapictUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Instapict_InstapictUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Instapict_InstapictUITests
+@end
---
+++ Pods/Target Support Files/Pods-InstapictTests/Pods-InstapictTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InstapictTests : NSObject
+@end
+@implementation PodsDummy_Pods_InstapictTests
+@end
