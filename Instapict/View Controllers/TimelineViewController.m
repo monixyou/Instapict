096573f7c2d6ada5a27363c69e7ccc10e6676099
Monica Bui <monicahb@fb.com>
2020-07-07 21:04:16 -0700

    Logout user

--- Instapict/View Controllers/TimelineViewController.m
+++ Instapict/View Controllers/TimelineViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TimelineViewController.h"
+#import <Parse/Parse.h>
+#import "SceneDelegate.h"
 
 @interface TimelineViewController ()
 
@@ -17,6 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapLogout:(id)sender {
+    __weak __typeof(self) weakSelf = self;
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        }
+        else {
+            // PFUser.current() will now be nil
+            SceneDelegate *sceneDelegate = (SceneDelegate *) weakSelf.view.window.windowScene.delegate;
+
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            UIViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+            sceneDelegate.window.rootViewController = loginViewController;
+        }
+    }];
 }
 
 /*
